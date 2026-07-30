1130ed1b82877f25ba99cfe5bc6df87b13111f1a
nenduo <120603709+nenduo@users.noreply.github.com>
2026-07-30 21:09:17 +0000

    Fix import statement and update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import path from "path";
+mport path from "path";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-export default defineConfig({
-  base: '/Jogo-da-velha/',
-  plugins: [...],
-})
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -14,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Jogo-da-velha/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
